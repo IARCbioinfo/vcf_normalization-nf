96ec59af1aeebab12b9456923c1197dec8372aa9
Tiffany Delhomme <delhommet@students.iarc.fr>
2018-04-30 09:16:58 +0200

    add dag to deploy

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,8 @@ cd ~/vt-nf/
 git config --global user.email "delhommet@students.iarc.fr"
 git config --global user.name "Circle CI_$CIRCLE_PROJECT_REPONAME_$CIRCLE_BRANCH"
 git pull
+git add dag*
+git commit -m "Generated DAG [skip ci]"
+git push origin $CIRCLE_BRANCH
 
 curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/vt-nf/trigger/90ddfa83-2377-4251-ac76-e64a7638deb0/
